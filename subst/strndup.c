b74ec066b50d58b75cbe85204a604447ca6d71b3
Tomasz Sterna <tomek@xiaoka.com>
2007-05-14 21:55:30 +0000

    Another fix for lack of strndup() on *BSD

--- subst/strndup.c
+++ subst/strndup.c
@@ -1,8 +1,10 @@
+#ifndef HAVE_STRNDUP
+
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #endif
+#include <stddef.h>
 
-#ifndef HAVE_STRNDUP
 char *strndup(char *str, size_t len)
 {
     char *dup = (char *)malloc(len+1);
