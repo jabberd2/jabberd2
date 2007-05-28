c45b4f52ffb4ecd8ea561a315c087af73968946b
Adam Strzelecki <ono@java.pl>
2007-05-28 11:11:54 +0000

    Define MIO backend in the project. Update win32 package names.

--- win32/include/config.h
+++ win32/include/config.h
@@ -376,7 +376,10 @@
 /* #undef MIO_POLL */
 
 /* Define to 1 if you want to use 'select' for non-blocking I/O. */
-#define MIO_SELECT 1
+/* #undef MIO_SELECT */
+
+/* Define to 1 if you want to use WSAAsyncSelect for non-blocking I/O. */
+/* #undef MIO_WSASYNC */
 
 /* Define to 1 if you want to enable NAD pointer tracking. */
 /* #undef NAD_DEBUG */
@@ -391,13 +394,13 @@
 #define PACKAGE_NAME "jabberd"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "jabberd 2.0s4"
+#define PACKAGE_STRING "jabberd 2.1SVN"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "jabberd"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.0s4"
+#define PACKAGE_VERSION "2.1SVN"
 
 /* Define to 1 if you want to enable memory pool statistics. */
 /* #undef POOL_DEBUG */
