18407e8ad265866307d93e26525c6d3d2682016f
Tomasz Sterna <tomek@xiaoka.com>
2008-02-24 17:51:25 +0000

    Do not handle vCard request destined to full JIDs. Fixes #190

--- sm/mod_iq_vcard.c
+++ sm/mod_iq_vcard.c
@@ -313,8 +313,8 @@ static mod_ret_t _iq_vcard_pkt_user(mod_instance_t mi, user_t user, pkt_t pkt) {
     st_ret_t ret;
     pkt_t result;
 
-    /* only handle vcard sets and gets */
-    if((pkt->type != pkt_IQ && pkt->type != pkt_IQ_SET) || pkt->ns != ns_VCARD)
+    /* only handle vcard sets and gets without resource */
+    if((pkt->type != pkt_IQ && pkt->type != pkt_IQ_SET) || pkt->ns != ns_VCARD || pkt->to->node !='\0')
         return mod_PASS;
 
     /* error them if they're trying to do a set */
