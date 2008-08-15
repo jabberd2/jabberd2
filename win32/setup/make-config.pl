e739968626fc03801e19b5e8e4120a527692cb8b
Adam Strzelecki <ono@java.pl>
2008-08-15 08:42:44 +0000

    Change default SM storage driver to SQLite on Windows.

--- win32/setup/make-config.pl
+++ win32/setup/make-config.pl
@@ -8,6 +8,8 @@ s/localhost\.localdomain/localhost/gi;
 s/\@(?:bindir|sysconfdir|localstatedir)\@/./gi;
 s/\@(?:pkglibdir)\@/modules/gi;
 s/\/jabberd\/(?:pid|log|db)//gi;
+s/we use the MySQL driver for all storage/we use the SQLite driver for all storage/gi;
+s/<driver>mysql<\/driver>/<driver>sqlite<\/driver>/gi;
 s/<module>mysql<\/module>/<module>sqlite<\/module>/gi;
 s/type='syslog'/type='file'/gi;
 s/\s*<!--\s*\n(\s*<\w+[^>]*>[^<]*\.(?:log|pem)<\/\w+>)\s*\n\s*-->[^\n]*/\n$1/gi;
