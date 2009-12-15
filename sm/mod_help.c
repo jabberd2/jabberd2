ac5d4bad033d63926edb50726a4ac6ef5b56da81
Tomasz Sterna <tomek@xiaoka.com>
2009-12-15 20:38:34 +0000

    [cwave:r45:46] fix an infinite loop where the help admin messages sm (produces a help response to itself)

--- sm/mod_help.c
+++ sm/mod_help.c
@@ -94,15 +94,15 @@ static mod_ret_t _help_pkt_sm(mod_instance_t mi, pkt_t pkt)
     {
         if (jid_compare_full(pkt->from, jid) == 0) {
             /* make a copy of the nad so it can be dumped to a string */
-            nad_t copy = nad_copy(nad);
+            nad_t copy = nad_copy(pkt->nad);
             if (!copy) {
-                log_write(sm->log, LOG_ERR "%s:%d help admin %s is messaging sm for help! packet dropped. (unable to print packet - out of memory?)", ZONE, jid_full(jid));
+                log_write(mod->mm->sm->log, LOG_ERR, "%s:%d help admin %s is messaging sm for help! packet dropped. (unable to print packet - out of memory?)", ZONE, jid_full(jid));
                 continue;
             }
             char * xml;
             int len;
             nad_print(copy, 0, &xml, &len);
-            log_write(sm->log, LOG_ERR, "%s:%d help admin %s is messaging sm for help! packet dropped: \"%.*s\"\n", ZONE, jid_full(jid), len, xml);
+            log_write(mod->mm->sm->log, LOG_ERR, "%s:%d help admin %s is messaging sm for help! packet dropped: \"%.*s\"\n", ZONE, jid_full(jid), len, xml);
             nad_free(copy);
             continue;
         }
