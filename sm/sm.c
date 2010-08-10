ea4252e69e737372c8fc8a74ef6d5eecfc1e1202
Tomasz Sterna <tomek@xiaoka.com>
2010-08-10 17:15:40 +0000

    Fixed incorrect order of arguments in debug call

--- sm/sm.c
+++ sm/sm.c
@@ -138,7 +138,7 @@ int sm_sx_callback(sx_t s, sx_event_t e, void *data, void *arg) {
                 elem = nad_append_elem(nad, ns, "bind", 0);
                 nad_set_attr(nad, elem, -1, "name", domain, len);
                 nad_append_attr(nad, -1, "multi", "to");
-                log_debug(ZONE, "requesting domain bind for '%.*s'", domain, len);
+                log_debug(ZONE, "requesting domain bind for '%.*s'", len, domain);
                 sx_nad_write(sm->router, nad);
             
             } while(xhash_iter_next(sm->hosts));
